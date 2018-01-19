c2b0666264d477514e80c0dc9d1c216bab4e502c
Russell McOrmond <russell.mcormond@canadiana.ca>
2018-01-19 09:54:49 -0500

    Replaced hard-coded options with Getopt::Long

--- lib/CIHM/TDR.pm
+++ lib/CIHM/TDR.pm
@@ -31,11 +31,11 @@ CIHM::TDR - Trustworthy Digital Repository management tools
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 
 sub new {
