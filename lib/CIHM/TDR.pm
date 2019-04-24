0989c1fd144818be4986f405b9bdf87e1662f001
Russell McOrmond <rmcormond@crkn.ca>
2019-04-24 10:44:06 -0400

    'tdrmeta' database removed.

--- lib/CIHM/TDR.pm
+++ lib/CIHM/TDR.pm
@@ -31,11 +31,11 @@ CIHM::TDR - Trustworthy Digital Repository management tools
 
 =head1 VERSION
 
-Version 0.12
+Version 0.13
 
 =cut
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 
 sub new {
